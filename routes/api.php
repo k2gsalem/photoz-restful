95b547a7da7db77b85f17e9feeb5a5b04f6ae24b
k2gsalem <k2gsalem@gmail.com>
2020-04-07 22:40:48 +0530

    Route Camel Case

--- routes/api.php
+++ routes/api.php
@@ -30,32 +30,32 @@ Route::group(['middleware' => 'auth:api'], function(){  //Authenticated only
 
     //Users
      //getprofile with auth
-    Route::put('/user/updateprofile','Api\UserController@updateprofile'); // put profile with auth
+    Route::put('/user/updateProfile','Api\UserController@updateprofile'); // put profile with auth
 
 
-    Route::get('/admin/user/getalluserinfo','Api\UserController@index'); // get all user info
-    Route::delete('/admin/user/deleteuser/{id}','Api\UserController@destroy'); //delete user with id
+    Route::get('/admin/user/getAllUserInfo','Api\UserController@index'); // get all user info
+    Route::delete('/admin/user/deleteUser/{id}','Api\UserController@destroy'); //delete user with id
 
     // Route::put('/users/{id}','Api\UserController@update'); //update user with id
     // Route::delete('/users/{id}','Api\USerController@destroy'); //delele user with id
 
     //Albums
-    Route::post('/admin/gallery/createalbum','Api\AlbumController@store');   //create new album
-    Route::put('/admin/gallery/updatealbum/{id}','Api\AlbumController@update'); //update album with id
-    Route::delete('/admin/gallery/deletealbum/{id}','Api\AlbumController@destroy'); //delele album with id
+    Route::post('/admin/gallery/createAlbum','Api\AlbumController@store');   //create new album
+    Route::put('/admin/gallery/updateAlbum/{id}','Api\AlbumController@update'); //update album with id
+    Route::delete('/admin/gallery/deleteAlbum/{id}','Api\AlbumController@destroy'); //delele album with id
 
 
     //Photos
-    Route::post('/admin/gallery/createphotos','Api\PhotoController@store');   //upload new photo
-    Route::put('/admin/gallery/updatephotos/{id}','Api\PhotoController@update'); //update photo with id
-    Route::delete('/admin/gallery/deletephotos/{id}','Api\PhotoController@destroy'); //delele photo with id
+    Route::post('/admin/gallery/createPhotos','Api\PhotoController@store');   //upload new photo
+    Route::put('/admin/gallery/updatePhotos/{id}','Api\PhotoController@update'); //update photo with id
+    Route::delete('/admin/gallery/deletePhotos/{id}','Api\PhotoController@destroy'); //delele photo with id
 
-    Route::post('/admin/gallery/createvideos','Api\VideoController@store');   //upload new photo
-    Route::put('/admin/gallery/updatevideos/{id}','Api\VideoController@update'); //update photo with id
-    Route::delete('/admin/gallery/deletevideos/{id}','Api\VideoController@destroy'); //delele photo with id
+    Route::post('/admin/gallery/createVideos','Api\VideoController@store');   //upload new photo
+    Route::put('/admin/gallery/updateVideos/{id}','Api\VideoController@update'); //update photo with id
+    Route::delete('/admin/gallery/deleteVideos/{id}','Api\VideoController@destroy'); //delele photo with id
 
-    Route::post('/admin/testimonial/writereview', 'Api\TestimonialsController@store');
-    Route::put('/admin/testimonial/editreview/{id}', 'Api\TestimonialsController@update');
+    Route::post('/admin/testimonial/writeReview', 'Api\TestimonialsController@store');
+    Route::put('/admin/testimonial/editReview/{id}', 'Api\TestimonialsController@update');
     Route::delete('/admin/testimonial/deleteReview/{id}', 'Api\TestimonialsController@destroy');//dek
 
 
@@ -67,9 +67,9 @@ $middleware = ['api'];
 if (\Request::header('Authorization'))
    $middleware = array_merge(['auth:api']);
 Route::group(['middleware' => $middleware], function () {
-    Route::get('/getallreview','Api\TestimonialsController@getallreview');
-    Route::get('/getallalbums','Api\AlbumController@getallalbums');
-    Route::get('/user/getprofile','Api\UserController@getprofile');
+    Route::get('/getAllReview','Api\TestimonialsController@getallreview');
+    Route::get('/getAllAlbums','Api\AlbumController@getallalbums');
+    Route::get('/user/getProfile','Api\UserController@getprofile');
 
     Route::get('/review/{id}', 'Api\TestimonialsController@show');
     Route::get('/users/{id}', 'Api\UserController@show');   //with auth -> return all albums, guest-> return only public albums
